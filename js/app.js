577b0d0b45faf97fe824ac4ec63aff5f41e0cc63
Tanni-Dey <tonnidey277615@gmail.com>
2022-03-01 15:14:53 +0600

    3rd-commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('first')
+const searchInput = document.getElementById('search-input');
+const loadPhone = () => {
+    const searchText = searchInput.value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhone(data))
+    searchInput.value = ''
+}
+const displayPhone = AllData => {
+    console.log(AllData)
+    if (AllData.status === false) {
+        document.getElementById('phone-empty-error').style.display = 'block'
+        searchInput.style.border = '1px solid red'
+    }
+}
