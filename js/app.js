b413bc0de6fc3c2266393161ab0e3b3a541c3c27
Tanni-Dey <tonnidey277615@gmail.com>
2022-03-01 18:26:22 +0600

    commit

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ const displayPhone = AllData => {
     }
     else {
         AllData.forEach(data => {
-            console.log(data)
+            //console.log(data)
             const div = document.createElement('div');
             div.classList.add('card')
             div.classList.add('p-4')
@@ -33,10 +33,33 @@ const displayPhone = AllData => {
             <div class="card-body">
               <h5 class="card-title">${data.phone_name}</h5>
               <p class="card-text">Brand : ${data.brand}</p>
-              <a href="#" class="btn btn-warning">Phone Details</a>
+              <button onclick="loadSinglePhone('${data.slug}')" class="btn btn-warning">Phone Details</button>
             </div>`
             allCard.appendChild(div);
             errorDisplay('none', '1px solid gainsboro')
         });
     }
+}
+// single phone details
+const loadSinglePhone = slug => {
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySinglePhone(data.data))
+}
+//display single phone details
+const displaySinglePhone = data => {
+    console.log(data)
+    const singleCard = document.getElementById('single-card')
+    singleCard.textContent = ''
+    const div = document.createElement('div');
+    div.classList.add('card')
+    div.classList.add('p-4')
+    div.innerHTML = `<img src="${data.image}" class="card-img-fluid" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${data.name}</h5>
+              <p class="card-text">Brand : ${data.brand}</p>
+              <p>${data.releaseDate ? data.releaseDate : 'No Date Found'}</p>
+            </div>`
+    singleCard.appendChild(div);
 }
